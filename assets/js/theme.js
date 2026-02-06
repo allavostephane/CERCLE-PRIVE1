adf935fe8c38e2879f48d6b7d44cb48037e09f81
allavostephane <info.allavostephane@gmail.com>
2026-02-06 17:42:33 +0100

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,25 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.getElementById("theme-toggle");
-  const body = document.body;
+    const toggle = document.getElementById("theme-toggle");
+    const body = document.body;
 
-  // Vérifier thème sauvegardé
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme) {
-    body.classList.remove("dark", "light");
-    body.classList.add(savedTheme);
-    toggle.checked = savedTheme === "light";
-  }
-
-  // Changer le thème
-  toggle.addEventListener("change", () => {
-    if (toggle.checked) {
-      body.classList.remove("dark");
-      body.classList.add("light");
-      localStorage.setItem("theme", "light");
-    } else {
-      body.classList.remove("light");
-      body.classList.add("dark");
-      localStorage.setItem("theme", "dark");
+    // Vérifie le thème sauvegardé
+    const savedTheme = localStorage.getItem("theme");
+    if(savedTheme){
+        body.classList.remove("dark", "light");
+        body.classList.add(savedTheme);
+        toggle.checked = savedTheme === "light" ? true : false;
     }
-  });
+
+    // Switcher mode jour/nuit
+    toggle.addEventListener("change", () => {
+        if(toggle.checked){
+            body.classList.remove("dark");
+            body.classList.add("light");
+            localStorage.setItem("theme", "light");
+        } else {
+            body.classList.remove("light");
+            body.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+        }
+    });
 });
