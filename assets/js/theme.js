c35df6f00bda37a75681517226b10fd2992ef51c
allavostephane <info.allavostephane@gmail.com>
2026-02-06 20:07:12 +0100

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,18 +1,18 @@
-const toggle = document.getElementById("theme-toggle");
-
 // Init theme
-const savedTheme = localStorage.getItem("theme");
-if(savedTheme){
-    document.body.className = savedTheme;
-    toggle.checked = savedTheme === "light";
-}
+let savedTheme = localStorage.getItem("theme");
+if(savedTheme) document.body.className = savedTheme;
+const toggle = document.getElementById("theme-toggle");
+toggle.checked = document.body.classList.contains("light");
 
+// Changement du mode
 toggle.addEventListener("change", () => {
     if(toggle.checked){
-        document.body.className = "light";
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
         localStorage.setItem("theme", "light");
     } else {
-        document.body.className = "dark";
+        document.body.classList.remove("light");
+        document.body.classList.add("dark");
         localStorage.setItem("theme", "dark");
     }
 });
