762b2ae01de1143175fe86e73838582752862e08
allavostephane <info.allavostephane@gmail.com>
2026-02-06 19:54:06 +0100

    Update age.js

--- assets/js/age.js
+++ assets/js/age.js
@@ -1,23 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     const popup = document.getElementById("age-popup");
-    const acceptBtn = document.getElementById("accept-age");
-    const declineBtn = document.getElementById("decline-age");
+    const accept = document.getElementById("accept-age");
+    const decline = document.getElementById("decline-age");
 
-    // Si l'utilisateur a déjà confirmé son âge, on cache le popup
-    if (!localStorage.getItem("ageConfirmed")) {
+    if(!localStorage.getItem("ageConfirmed")){
         popup.classList.add("active");
         document.body.style.overflow = "hidden";
     }
 
-    // Bouton accepter
-    acceptBtn.addEventListener("click", () => {
+    accept.addEventListener("click", () => {
         localStorage.setItem("ageConfirmed", "true");
         popup.classList.remove("active");
         document.body.style.overflow = "auto";
     });
 
-    // Bouton quitter
-    declineBtn.addEventListener("click", () => {
+    decline.addEventListener("click", () => {
         window.location.href = "https://www.google.com";
     });
 });
